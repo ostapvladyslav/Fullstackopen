04f63624d1c3c1206c138f20e36b5846f9edb629
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-17 03:39:30 +0200

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,36 +5,38 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  let average = all < 1 ? 0 : (good * 1 - bad * 1) / all;
+  let positive = all < 1 ? 0 : good / all;
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button
-        onClick={() => {
-          setGood(good + 1);
-        }}
-      >
-        good
-      </button>
-      <button
-        onClick={() => {
-          setNeutral(neutral + 1);
-        }}
-      >
-        neutral
-      </button>
-      <button
-        onClick={() => {
-          setBad(bad + 1);
-        }}
-      >
-        bad
-      </button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
 
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   );
 };
